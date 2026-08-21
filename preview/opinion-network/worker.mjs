ade1744424468926b8fc44004b8ba2ab05c39c1a
annon <michi.unknown.zwei@gmail.com>
2026-08-21 09:20:11 +0900

    Route opinion preview root to preview page

--- preview/opinion-network/worker.mjs
+++ preview/opinion-network/worker.mjs
@@ -1,5 +1,10 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === "/") {
+      url.pathname = "/chunk-network-entanglement-preview.html";
+      return env.ASSETS.fetch(new Request(url.toString(), request));
+    }
     return env.ASSETS.fetch(request);
   },
 };
